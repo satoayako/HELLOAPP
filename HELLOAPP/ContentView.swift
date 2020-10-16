4caf81e7b3cdcc2c7af98907187325d75a2cd9a4
ayako_sato <ayako.sato@d2cdot.co.jp>
2020-10-16 15:15:47 +0900

    Initial Commit

---
+++ HELLOAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HELLOAPP
+//
+//  Created by 佐藤 順子 on 2020/10/16.
+//  Copyright © 2020 佐藤 順子. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
